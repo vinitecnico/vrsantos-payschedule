436e6cea9076854f7ec565fb81585c66caf681dd
vinicius rodrigues dos santos <vrsantos@br.ibm.com>
2016-07-22 10:44:08 -0300

    commit

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const http = require('http');
+var express = require('express');
+var app = express();
+var cfenv = require('cfenv');
+var bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use(express.static(__dirname + '/public'));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port, '0.0.0.0', function() {
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
