6b0bc99a6e04696640d06bd95b550fdab58ddcc3
vinitecnico <vinitecnico@gmail.com>
2016-07-24 21:14:32 -0300

    fix

--- public/app/user/user.login.component.ts
+++ public/app/user/user.login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     //selector: 'talk-list',
-    templateUrl: 'app/pessoa/pessoa.component.html'
+    templateUrl: 'app/user/html/user.login.component.html'
 })
 export class UserLoginComponent {
     title = 'Welcome to Solar System Planets !!';
